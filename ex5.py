d2548b29999a064a25d74cb29e5e59286d2609f2
kghorbanzadeh23 <kamand.ghorbanzadeh@ucalgary.ca>
2024-03-22 15:00:26 -0600

    ex5 is partial done

--- ex5.py
+++ ex5.py
@@ -1,2 +1,15 @@
 # Topological sorting can be implemented using Depth-First Search (DFS).
-# The DFS is suitable for topological sorting because they can 
+# Finding Cycles: Only Directed Acyclic Graphs (DAGs) allow for topological
+# sorting. A graph's cycles may be found using DFS, which is important because
+# a topological sort cannot be completed in the presence of a cycle.
+
+# Ordering: Before turning around, DFS travels as far as feasible down each branch.
+# This parameter makes sure that all outgoing edges from a node are visited before the
+# node is closed, meaning that all descendants of a given node are visited before the node
+# is finished. This is a useful characteristic since a node in a topological sort has to come
+# after all of its dependencies, or graph descendants.
+
+# Stack Usage: To preserve the vertex order in a DFS implementation for topological sorting,
+# a stack is usually employed. A vertex is pushed into the stack each time it is completed,
+# meaning that all of its edges have been investigated. The stack has a legitimate topological
+# ordering from the bottom to the top once every vertex has been visited.
